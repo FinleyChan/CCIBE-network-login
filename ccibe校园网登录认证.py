4ccf6e7898a7414708e8361fc198e13f69d6b64c
FinleyChan <110627691+FinleyChan@users.noreply.github.com>
2023-09-10 17:30:28 +0800

    Update ccibe校园网登录认证.py
    1.  更改data参数
    2.  新增网络连接判断

--- ccibe校园网登录认证.py
+++ ccibe校园网登录认证.py
@@ -9,18 +9,28 @@
 import requests
 import re
 from time import sleep
+import subprocess
 
 # 用户名
 userId = ""
 # 密码
 passwd = ""
 
-pre_url = "http://www.msftconnecttest.com/redirect"
+# pre_url = "http://www.msftconnecttest.com/redirect"
+pre_url = "http://1.1.1.1"
 
 response = requests.get(pre_url)
-url = response.url.replace("portal", "webauth")
+referer = response.url
+url = referer.replace("portal", "webauth")
 cookie_dict = requests.utils.dict_from_cookiejar(response.cookies)
-cookie_str = [f"{key}={value}" for key, value in cookie_dict.items()][0]
+# cookie_str = [f"{key}={value}" for key, value in cookie_dict.items()][1]
+
+key = [f"{key}" for key in cookie_dict.keys()][1]
+value = [f"{value}" for value in cookie_dict.values()][1]
+
+# cookies = {'': 'null', key: value}
+cookies = {key: value}
+
 host = re.findall("//(.*?)/", url)[0]
 
 header = {
@@ -30,44 +40,48 @@ header = {
     "Accept-Language": "zh-CN,zh;q=0.9",
     "Cache-Control": "max-age=0",
     "Connection": "keep-alive",
-    "Content-Length": "398",
+    "Content-Length": "458",
     "Content-Type": "application/x-www-form-urlencoded",
-    "Cookie": f"{cookie_str}",
     "Host": f"{host}",
     "Origin": f"{host}",
-    "Referer": f"{url}",
+    "Referer": f"{referer}",
     "Upgrade-Insecure-Requests": "1",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 "
                   "Safari/537.36 Edg/114.0.0.0",
 }
 
 data = {
-    "hostIp": "http://127.0.0.1:8082/",
+    "scheme": "http",
+    "serverIp": "tomcat_server:80",
+    "hostIp": "http://127.0.0.1:8081/",
     "loginType": "",
     "auth_type": "0",
     "isBindMac1": "1",
-    "pageid": "1",
+    "pageid": "61",
     "templatetype": "1",
     "listbindmac": "1",
     "recordmac": "1",
     "isRemind": "0",
+    "url": "http://1.1.1.1",
     "isautoauth": "",
     "notice_pic_loop1": "/portal/uploads/pc/demo3/images/logo.jpg",
     "notice_pic_loop2": "/portal/uploads/pc/demo3/images/rrs_bg.jpg",
     "userId": userId,
     "passwd": passwd,
-    "twiceauth": "1",
-    "isBindMac": "bindmac",
+    "remInfo": "on",
+    "isBindMac": "bindmac"
 }
 
-while True:
+if __name__ == '__main__':
     try:
-        response_auth = requests.post(url=url, headers=header, data=data)
-        if response_auth.status_code == 200:
-            print("认证成功")
+        response_auth = requests.post(url=url, headers=header, data=data, cookies=cookies)
+        # connection_stat = os.system('ping www.baidu.com -n 2')
+        connection_stat = subprocess.run("ping www.baidu.com -n 3", stdout=subprocess.DEVNULL).returncode
+        if connection_stat == 0:
+            print("认证成功, 网络已连接！！！")
             sleep(3)
-            break;
-    except Exception as e:
-        print("认证出错")
+        else:
+            raise ConnectionError("connect failed.")
+    except ConnectionError:
+        print("认证出错！！！")
         sleep(3)
-        break;
